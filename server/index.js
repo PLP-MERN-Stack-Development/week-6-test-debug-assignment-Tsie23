38a7ea8118a6011e6a8c98d0d0495739df564444
Tsie23 <ka.sennelo@gmail.com>
2025-07-14 15:13:27 +0200

    Updated package.json files in client and server side Created index.js and setupTestDb.js in server side

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/myapp';
+
+mongoose.connect(MONGO_URI).then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
---
+++ server/scripts/setupTestDb.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const TEST_DB_URI = process.env.TEST_DB_URI || 'mongodb://localhost:27017/test-db';
+
+mongoose.connect(TEST_DB_URI).then(() => {
+  console.log('Connected to test DB');
+  mongoose.connection.dropDatabase().then(() => {
+    console.log('Test DB cleaned');
+    process.exit(0);
+  });
+});
